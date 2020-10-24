ca7a7e5fd7969d73dae6407e26747971348c8492
tsabz <tonia.saba@gmail.com>
2020-10-24 15:02:46 -0400

    notes on ASCII

---
+++ funny_string.py
@@ -0,0 +1,9 @@
+#ASCII - using numbers to represent text
+    #it's a ways for computer to store characters easily
+    #in its memory by convertiving them to numbers
+    # uppercase A is always asigned the number 65
+    # ! is always number 33
+    # ASCII Charactars - 128 
+    # Unicode Charactars - 1,114,112
+    # Lowercase a (97) - uppercase A (65) = 32
+    # Lowercase b (98) - uppcaser B (66) = 32
