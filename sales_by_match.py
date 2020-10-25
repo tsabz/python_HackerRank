0531ff201a10f98ffdccc0f7bed13183ade726bb
tsabz <tonia.saba@gmail.com>
2020-10-25 18:41:33 -0400

    pseudocoded idea for sales by match code

---
+++ sales_by_match.py
@@ -0,0 +1,11 @@
+# Theres an array of socks, we must find the number of pairs there are.
+
+# n = 7 , length of the array, number of socks in a pile [1,2,1,2,1,3,2]
+#must itterate through the array and find a the individual colors and their pairs
+#create a hash table
+# have two counters 
+# add a counter , once the counter reaches two  add to the hash table
+# if it equals two it is a pair add to pair counter print(pairs)
+#
+# 
+#  n = len(ar)+1
